c3a8159bb00c5a1b28b664791f33e2219a08e803
Muhammad Hamza <muhammadhamza514@gmail.com>
2022-03-29 14:47:49 +0500

    Assignment 01 Complete

---
+++ public/app.js
@@ -0,0 +1,33 @@
+// pass by reference
+// let a={
+//     username:"Osama"
+// }
+// let b={
+//     username:"hamza"
+// }
+// b=a
+// b.username="agha"
+// console.log(b)
+// console.log(a)
+
+// pass by value
+// let c=5;
+// let z=c+5;
+
+// console.log(c)
+// console.log(z)
+
+
+
+const human = {  
+    firstName: 'Muhammad',  
+    lastName: 'Hamza',
+    program: 'W&M application',  
+    institute: 'JAWAN PAKISTAN'    
+};  
+const propertykeys=Object.keys(human);  
+const propertyNames=Object.values(human);  
+console.log(propertykeys);  
+console.log(propertyNames);  
+
+
